afb5ede25beb0e44135d824b1f97a735bcf4afdb
naufal-al-munawar251 <naufalalmunawar251@gmail.com>
2022-10-06 10:33:16 +0700

    first commit

---
+++ index.java
@@ -0,0 +1,8 @@
+class index{
+    public static void main(String[] args) {
+        System.out.println("Reseb krabby patty");
+        System.out.println("=========================");
+        System.out.println("1. Barang");
+        System.out.println("2. Ketimun");
+    }
+}
